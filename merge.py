08859ef35cfb5470f80bdd1158f3129f61e9a12f
Tnvi-23 <tanvibidve6@gmail.com>
2025-12-23 16:53:26 +0000

    merge two arrays

---
+++ merge.py
@@ -0,0 +1,13 @@
+def merge_arrays(arr1, arr2):
+    merged = []
+    for i in arr1:
+        merged.append(i)
+    for j in arr2:
+        merged.append(j)
+    return merged
+
+# --- Main Program ---
+arr1 = [10, 20, 30]
+arr2 = [40, 50, 60]
+
+print("Merged array:", merge_arrays(arr1, arr2))
